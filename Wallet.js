4b99254f751fcdc714d76db999153da39faf5790
Vladislav <kudr9tov@gmail.com>
2021-01-02 14:36:06 +0300

    widgets

--- Wallet.js
+++ Wallet.js
@@ -7,32 +7,35 @@ import {
 
 const Wallet = ({item}) => (
     <li aria-hidden="false">
-        <div className={['income-item', item.theme].join(" ")}>
-            <div className="income-item-title">
-                <div className="income-item-name left"><Text>{item.name}</Text></div>
-                <div className="income-item-type right"><View>{item.type === 'visa' ? <i className="fab fa-cc-visa"/> :
-                    <i className="fas fa-piggy-bank"/>}</View></div>
-            </div>
-            <div className="amount">
-                <span className="left"><Text>Баланс:</Text></span>
-                <div className="right">
-                    <span className="value"><Text>{item.balance.toFixed(2)}</Text></span>
-                    <span className="currency"><Text>{item.currency}</Text></span>
-                </div>
-            </div>
-            <div className="status">
-                <div className="income-value left">
-                    <span><Text>Доход:</Text></span>
-                    <span className="value"><Text>{item.income.toFixed(2)}</Text></span>
-                    <span className="currency"><Text>{item.currency}</Text></span>
-                </div>
-                <div className="spend-value right">
-                    <span><Text>Расход:</Text></span>
-                    <span className="value"><Text>{item.spend.toFixed(2)}</Text></span>
-                    <span className="currency"><Text>{item.currency}</Text></span>
-                </div>
-            </div>
-        </div>
+        <View className={['income-item', item.theme].join(" ")}>
+            <View className="income-item-title">
+                <View className="income-item-name left"><Text>{item.name}</Text></View>
+                <View className="income-item-type right">
+                    {item.type === 'visa'
+                        ? <i className="fab fa-cc-visa"/>
+                        : <i className="fas fa-piggy-bank"/>}
+                </View>
+            </View>
+            <View className="amount">
+                <Text className="left">Баланс:</Text>
+                <View className="right">
+                    <Text className="value">{item.balance.toFixed(2)}</Text>
+                    <Text className="currency">{item.currency}</Text>
+                </View>
+            </View>
+            <View className="status">
+                <View className="income-value left">
+                    <Text>Доход:</Text>
+                    <Text className="value">{item.income.toFixed(2)}</Text>
+                    <Text className="currency">{item.currency}</Text>
+                </View>
+                <View className="spend-value right">
+                    <Text>Расход:</Text>
+                    <Text className="value">{item.spend.toFixed(2)}</Text>
+                    <Text className="currency">{item.currency}</Text>
+                </View>
+            </View>
+        </View>
     </li>
 );
 
